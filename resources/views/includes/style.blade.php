ad3a3075fa7ac7b01aba73beca3ca5e7fbcb3e26
irfanripat <irfanripat@gmail.com>
2021-01-19 23:28:20 +0800

    first commit

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,12 @@
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+<link rel="stylesheet" href="{{ url('nitro2/fonts/icomoon/style.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/jquery-ui.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/jquery.fancybox.min.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/bootstrap-datepicker.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/fonts/flaticon/font/flaticon.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/aos.css') }}">
+<link rel="stylesheet" href="{{ url('nitro2/css/style.css') }}">
