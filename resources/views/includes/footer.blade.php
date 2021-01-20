fcbb571a67723602f628150844966e5039854a27
irfanripat <irfanripat@gmail.com>
2021-01-20 08:23:34 +0800

    finish create footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,40 +1,35 @@
 <footer class="site-footer">
-    <div class="container">
+      <div class="container">
         <div class="row">
-          <div class="col-md-9">
+          <div class="col-md-12">
             <div class="row">
               <div class="col-md-5">
-                <h2 class="footer-heading mb-4">About Us</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
+                <h2 class="footer-heading mb-4 font-weight-bold">Tentang Kami</h2>
+                <p>Website Resmi Pemerintah Desa Kurrusumanga, Keacamatan Belopa, Kabupaten Luwu.</p>
               </div>
               <div class="col-md-3 ml-auto">
-                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <h2 class="footer-heading mb-4 font-weight-bold">Link Cepat</h2>
                 <ul class="list-unstyled">
-                  <li><a href="#about-section" class="smoothscroll">About Us</a></li>
-                  <li><a href="#services-section" class="smoothscroll">Services</a></li>
-                  <li><a href="#testimonials-section" class="smoothscroll">Testimonials</a></li>
-                  <li><a href="#contact-section" class="smoothscroll">Contact Us</a></li>
+                  <li><a href="#about-section" class="smoothscroll">Profil Desa</a></li>
+                  <li><a href="#services-section" class="smoothscroll">Potensi Desa</a></li>
+                  <li><a href="#testimonials-section" class="smoothscroll">Berita Desa</a></li>
+                  <li><a href="#testimonials-section" class="smoothscroll">Dana Desa</a></li>
+                  <li><a href="#contact-section" class="smoothscroll">Hubungi Kami</a></li>
                 </ul>
               </div>
-              <div class="col-md-3">
-                <h2 class="footer-heading mb-4">Follow Us</h2>
+              <div class="col-md-4">
+                <h2 class="footer-heading mb-4 font-weight-bold">Hubungi Kami</h2>
+                <ul class="list-unstyled">
+                  <li>Phone : 085256145698</li>
+                  <li>Email : <a href="mailto:kurrusumanga@desa.id">kurrusumanga@desa.id</a></li>
+                  <li>Alamat : Desa Kurrusumanga, Belopa</li>
+                </ul>
                 <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
                 <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
                 <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
                 <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
               </div>
             </div>
-          </div>
-          <div class="col-md-3">
-            <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
-            <form action="#" method="post" class="footer-subscribe">
-              <div class="input-group mb-3">
-                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
-                <div class="input-group-append">
-                  <button class="btn btn-primary text-black" type="button" id="button-addon2">Send</button>
-                </div>
-              </div>
-            </form>
           </div>
         </div>
         <div class="row pt-5 mt-5 text-center">
