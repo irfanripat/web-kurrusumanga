4144f57a5fa76ab5eb96f9252c9d5a180eefcf0e
irfanripat <irfanripat@gmail.com>
2021-01-20 09:10:31 +0800

    finish create header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
         <div class="row align-items-center">
           
           <div class="col-6 col-xl-2">
-            <h1 class="mb-0 site-logo"><a href="index.html" class="h2 mb-0">Nitro<span class="text-primary">.</span> </a></h1>
+            <h1 class="mb-0 site-logo"><a href="welcom.html" class="h2 mb-0"><span class="text-primary">Kurrusumanga</span> </a></h1>
           </div>
 
           <div class="col-12 col-md-10 d-none d-xl-block">
@@ -13,11 +13,12 @@
               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
                 <li><a href="#home-section" class="nav-link">Home</a></li>
                 <li class="has-children">
-                  <a href="#about-section" class="nav-link">About Us</a>
+                  <a href="#about-section" class="nav-link">Profil Desa</a>
                   <ul class="dropdown">
-                    <li><a href="#team-section" class="nav-link">Team</a></li>
-                    <li><a href="#pricing-section" class="nav-link">Pricing</a></li>
-                    <li><a href="#faq-section" class="nav-link">FAQ</a></li>
+                    <li><a href="#team-section" class="nav-link">Informasi Geografis</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Struktur Pemerintahan</a></li>
+                    <li><a href="#portfolio-section" class="nav-link">Potensi Desa</a></li>
+                    <!-- <li><a href="#faq-section" class="nav-link">FAQ</a></li>
                     <li class="has-children">
                       <a href="#">More Links</a>
                       <ul class="dropdown">
@@ -25,15 +26,12 @@
                         <li><a href="#">Menu Two</a></li>
                         <li><a href="#">Menu Three</a></li>
                       </ul>
-                    </li>
+                    </li> -->
                   </ul>
                 </li>
-                
-                <li><a href="#portfolio-section" class="nav-link">Portfolio</a></li>
-                <li><a href="#services-section" class="nav-link">Services</a></li>
-                <li><a href="#testimonials-section" class="nav-link">Testimonials</a></li>
-                <li><a href="#blog-section" class="nav-link">Blog</a></li>
-                <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                <li><a href="#blog-section" class="nav-link">Berita Desa</a></li>
+                <li><a href="#services-section" class="nav-link">Dana Desa</a></li>
+                <li><a href="#contact-section" class="nav-link">Hubungi Kami</a></li>
               </ul>
             </nav>
           </div>
